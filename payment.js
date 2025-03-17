c03c468ece313fc66e9a2ae98fd2e7576718d44f
Kodam Sai Charan <96488659+Saicharan2k2@users.noreply.github.com>
2025-03-17 11:38:03 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -5,14 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault(); // Prevent default form submission
 
-    // Check form validity
     if (form.checkValidity()) {
-      // Hide the form and show the success message
       form.style.display = 'none';
       successMessage.classList.remove('hidden');
       successMessage.classList.add('visible');
-
-      // Redirect back to the home page after 3 seconds
       setTimeout(() => {
         window.location.href = 'index.html';
       }, 5000);
